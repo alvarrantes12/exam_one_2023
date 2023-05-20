06c4d540821d5c358ce94d72ec0e7fabfb404c0b
Daniel <daniel.vargas@ucrso.info>
2023-05-20 10:10:50 -0600

    Pokemon trainer CRUD Done

--- app/controllers/pokemon_trainers_controller.rb
+++ app/controllers/pokemon_trainers_controller.rb
@@ -17,7 +17,7 @@ class PokemonTrainersController < ApplicationController
     @pokemon_trainer = PokemonTrainer.new(pokemon_trainer_params)
 
     if @pokemon_trainer.save
-      redirect_to pokemon_trainer_url(@pokemon_trainer), notice: "Pokemon trainer was successfully created."
+      redirect_to pokemon_trainer_url(@pokemon_trainer), notice:  t('application.create_message', model: t('activerecord.modules.pokemon_trainer.one'))
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,7 +25,7 @@ class PokemonTrainersController < ApplicationController
 
   def update
     if @pokemon_trainer.update(pokemon_trainer_params)
-      redirect_to pokemon_trainer_url(@pokemon_trainer), notice: "Pokemon trainer was successfully updated."
+      redirect_to pokemon_trainer_url(@pokemon_trainer), notice: t('application.update_message', model: t('activerecord.modules.pokemon_trainer.one'))
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,7 +35,7 @@ class PokemonTrainersController < ApplicationController
   def destroy
     @pokemon_trainer.destroy
 
-   redirect_to pokemon_trainers_url, notice: "Pokemon trainer was successfully destroyed."
+   redirect_to pokemon_trainers_url, notice: t('application.delete_message', model: t('activerecord.modules.pokemon_trainer.one'))
   end
 
   private
@@ -45,6 +45,6 @@ class PokemonTrainersController < ApplicationController
   end
 
   def pokemon_trainer_params
-    params.require(:pokemon_trainer).permit(:first_name, :last_name, :region, :id_number)
+    params.require(:pokemon_trainer).permit(:first_name, :last_name, :region, :id_number, :level)
   end
 end
--- spec/models/pokemon_trainer_spec.rb
+++ spec/models/pokemon_trainer_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe PokemonTrainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(first_name: "Ramirez",
+                        last_name: "Domingo",
+                        region: "Guatemala",
+                        id_number: "RDG1234567",
+                        level: 1)
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end  
+
+  it "is not valid without first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without id_number" do
+    subject.id_number = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without level" do
+    subject.level = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a first_name with more than 10 letters" do
+    subject.first_name = "asdfghjklvsvsvsvsfg"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a last_name with more than 10 letters" do
+    subject.last_name = "asdfghjklvsvsvsvsfg"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a id_number with less than 8 letters" do
+    subject.id_number = "asdfghj"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a id_number with more than 10 letters" do
+    subject.id_number = "asdfghjklvsvsvsvsfg"
+    expect(subject).to_not be_valid
+  end
+
 end
