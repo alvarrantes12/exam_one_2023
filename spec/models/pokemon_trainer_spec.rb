c24f4ceb1f904ff91d5921793a31cea777a01af2
AlexaVegaRojas <alexavr92@gmail.com>
2023-05-20 11:27:15 -0600

    avr-C09317 PokemontrainerCRUDdone

--- spec/models/pokemon_trainer_spec.rb
+++ spec/models/pokemon_trainer_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe PokemonTrainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(first_name: "Pokemon",
+                        last_name: "Pokemon",
+                        region: "Central-Costa Rica",
+                        id_number: "11111111",
+                        level: 0)
+  }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without id_number" do
+    subject.id_number = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a first_name with more then 10 letters" do
+    subject.first_name = "PokemonPokemonPokemon"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a last_name with more then 10 letters" do
+    subject.last_name = "PokemonPokemonPokemon"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a id_number with less then 8 numbers" do
+    subject.id_number = "1234"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a id_number with more then 10 numbers" do
+    subject.id_number = "1234567891011"
+    expect(subject).to_not be_valid
+  end
 end
+
