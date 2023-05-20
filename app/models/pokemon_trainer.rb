be0dbdac575c5c6615e69c326c63c1b57f70dddc
jesus <jeadroar05@gmail.com>
2023-05-20 10:40:38 -0600

    Pokemon trainer CRUD done

---
+++ app/models/pokemon_trainer.rb
@@ -0,0 +1,9 @@
+class PokemonTrainer < ApplicationRecord
+    
+    validates :first_name, presence: true, length: {maximum:10}
+    validates :last_name, presence: true, length: {maximum:10}
+    validates :id_number, presence: true, uniqueness: true, length: {minimum:8, maximum:10}
+    validates :level, presence: true
+
+    enum level: {'low': 0, 'medium': 1, 'high': 2}
+end
--- spec/models/pokemon_trainer_spec.rb
+++ spec/models/pokemon_trainer_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe PokemonTrainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { 
+    described_class.new(first_name: "Jesus",
+                        last_name: "Rodriguez",
+                        region: "Kanto",
+                        id_number: "208460338",
+                        level: 1)
+   }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without id_number" do
+    subject.id_number = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without level" do
+    subject.id_number = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a first_name with more than 10 letters" do
+    subject.first_name = "abcdefghijk"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a last_name with more than 10 letters" do
+    subject.last_name = "abcdefghijk"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a id_number with less than 8 numbers" do
+    subject.id_number = "1234567"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a id_number with more than 10 numbers" do
+    subject.id_number = "12345678912"
+    expect(subject).to_not be_valid
+  end
 end
